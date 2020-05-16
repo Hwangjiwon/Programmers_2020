5356fb71235bab10eb1f80d5334f8f803e641546
Jiwon <googims330@gmail.com>
2020-05-16 23:57:34 +0900

    Add files via upload

---
+++ hanoi/Main.java
@@ -0,0 +1,14 @@
+package hanoi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void move(int n, char from, char to) {
+		System.out.println(n + "이동 : " + from + " -> " + to);
+	}
+
+}

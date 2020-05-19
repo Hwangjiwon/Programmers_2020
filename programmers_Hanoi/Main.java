d3fd1e02d73df1a54d1f1bebfbde8e6f117970b4
Jiwon <googims330@gmail.com>
2020-05-20 00:03:42 +0900

    Update Main.java

--- programmers_Hanoi/Main.java
+++ programmers_Hanoi/Main.java
@@ -1,10 +1,17 @@
 package programmers_Hanoi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split("");
+		
+		br.close();
 	}
 
 	public void move(char a, char b, int n) {

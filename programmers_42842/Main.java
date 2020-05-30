33a54e4c5059f45fd1b84cbe10255906af20346b
Jiwon <googims330@gmail.com>
2020-05-30 23:26:29 +0900

    Create Main.java

---
+++ programmers_42842/Main.java
@@ -0,0 +1,14 @@
+package programmers_42842;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int[] solution(int brown, int yellow) {
+		int[] answer = {};
+		return answer;
+	}
+}

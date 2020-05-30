d65e0b2807a800d6fac75f94dfac80036b54b348
Jiwon <googims330@gmail.com>
2020-05-30 23:58:26 +0900

    Update Main.java

--- programmers_42842/Main.java
+++ programmers_42842/Main.java
@@ -1,14 +1,32 @@
 package programmers_42842;
 
 public class Main {
+	/*
+	 * 카펫 중앙은 노랑, 테두리 1줄은 갈색 노란색수, 갈색수는 알고 있지만 가로,세로길이는 모를때, 가로,세로크기를 순서대로 배열에 담아
+	 * return
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int b = 10;
+		int y = 2;
 
+		System.out.println(solution(b, y));
 	}
 
 	public static int[] solution(int brown, int yellow) {
 		int[] answer = {};
+
+		/*
+		 * $$$$$$% &@@@@@% ******%
+		 */
+
+		/*
+		 * $$$% &@@% &@@% &***
+		 */
+		int tmp;
+		int row, col;
+		tmp = brown * yellow;
 		return answer;
 	}
 }

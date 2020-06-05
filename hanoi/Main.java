ec93ddbeb644cb408ba54c7a4aa2cd969213704f
Jiwon <googims330@gmail.com>
2020-06-05 11:01:44 +0900

    하노이의 탑

--- hanoi/Main.java
+++ hanoi/Main.java
@@ -4,11 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = 3;
+		hanoi(n, 'a', 'b', 'c');
 	}
 
-	public void move(int n, char from, char to) {
-		System.out.println(n + "이동 : " + from + " -> " + to);
+	public static void move(char a, char b){
+		System.out.println(a + " -> " + b);
 	}
-
+	
+	public static void hanoi(int n, char from, char by, char to){
+		if(n == 1){
+			move(from, to);
+		}else {
+			hanoi(n-1, from, to, by);
+			move(from, to);
+			hanoi(n-1, by, from, to);
+		}
+	}
 }

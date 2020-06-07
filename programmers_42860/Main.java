23d6a5a22ecc89e72560806c67970ed608d5b0b8
Jiwon <googims330@gmail.com>
2020-06-08 00:14:39 +0900

    Create Main.java

---
+++ programmers_42860/Main.java
@@ -0,0 +1,17 @@
+package programmers_42860;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "JAZ";
+		
+		System.out.println(solution(name));
+	}
+	
+	public static int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+
+}
